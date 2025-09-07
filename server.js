c7a3f5980fe4efecb0b05bfff8119664cb3876e3
tirthgodhani <145262541+tirthgodhani@users.noreply.github.com>
2025-09-07 21:41:05 +0530

    Update server.js
    For CORS

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://profound-sprinkles-3f4a37.netlify.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
